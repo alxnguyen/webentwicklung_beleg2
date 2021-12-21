d704dbbb9ca46d92c3c6763ee93c5bae8efb4853
alxnguyen <alexander.nguyen1509@gmail.com>
2021-12-21 19:02:33 +0100

    removed console logs

--- script.js
+++ script.js
@@ -2,13 +2,11 @@ function myFunction() {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
     const json_data = JSON.stringify({email: email, password: password});
-    console.log(json_data);
     fetch("https://webentwicklung-beleg-backend.herokuapp.com/", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: json_data
     })
     .then(res => res.text())
-    .then(data => console.log(data))
     .catch(error => console.log(error));
 }
