96de2c1d7ec576ed954d1bb96967782065fd6941
alxnguyen <alexander.nguyen1509@gmail.com>
2022-01-17 17:25:45 +0100

    added register page

--- script.js
+++ script.js
@@ -10,3 +10,37 @@ function myFunction() {
     .then(res => res.text())
     .catch(error => console.log(error));
 }
+
+function myFunction2() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const password2 = document.getElementById("password2").value;
+    if(email.length === 0 || password.length === 0 || password2.length === 0) {
+        document.getElementById("warning").innerHTML = "Bitte füllen Sie alle Felder aus.";
+        return;
+    }
+    if(password != password2) {
+        document.getElementById("warning").innerHTML = "Passwörter stimmen nicht überein.";
+        return;
+    }
+    const json_data = JSON.stringify({email: email, password: password});
+    fetch("https://webentwicklung-beleg-backend.herokuapp.com/", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: json_data
+    })
+    .then(res => 
+        {
+            if (res.status === 201) {
+                window.location.href = "./register_success";
+            }
+            if(res.status === 500) {
+                document.getElementById("warning").innerHTML = "Irgendetwas ist schief gelaufen.";
+            }
+        })
+    .catch(error => console.log(error));
+}
+
+function myFunction3() {
+    window.location.href = "./index.html";
+}
